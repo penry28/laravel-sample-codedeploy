914b0db251da5b69ab81a7a09f62b1949e9b5ba9
howard_cba <howard_cba@cybridge.jp>
2022-03-21 16:39:05 +0700

    Fix deploy

--- .deploy/prod-autodeploy.sh
+++ .deploy/prod-autodeploy.sh
@@ -1,1 +1,1 @@
-cd /var/www/html/sample-codedeploy && git pull origin master && sudo yarn install && sudo yarn run dev && sudo php artisan route:clear && sudo php artisan view:clear && sudo php artisan config:clear && sudo chown -R nginx:nginx storage/ bootstrap/
+cd git pull origin master && sudo yarn install && sudo yarn run dev && sudo php artisan route:clear && sudo php artisan view:clear && sudo php artisan config:clear && sudo chown -R nginx:nginx storage/ bootstrap/
